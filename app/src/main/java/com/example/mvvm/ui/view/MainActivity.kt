61bee81842637ef55b8ca9cdbd4f60e24d5d9a66
dfchaves <davidchaves@tyba.com.co>
2021-12-04 10:05:10 -0500

    Add Progress Bar and define new Live Data and new Observer

--- app/src/main/java/com/example/mvvm/ui/view/MainActivity.kt
+++ app/src/main/java/com/example/mvvm/ui/view/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.mvvm.ui.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.core.view.isVisible
 import androidx.lifecycle.Observer
 // Import for ActivityMainBinding
 import com.example.mvvm.databinding.ActivityMainBinding
@@ -18,9 +19,18 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Call on create viewModel function to call first screen
+        quoteViewModel.onCreate()
+
+        // Create observer for Quote Models
         quoteViewModel.quoteModel.observe(this, Observer { currentQuote ->
             binding.tvQuote.text = currentQuote.quote
             binding.tvAutor.text = currentQuote.author
+        })
+
+        // Create observer for Progress Bar
+        quoteViewModel.isLoading.observe(this, Observer { currentFocus ->
+            binding.progressCircular.isVisible = currentFocus
         })
 
         binding.viewContainer.setOnClickListener { quoteViewModel.randomQuote() }
--- app/src/main/java/com/example/mvvm/ui/viewModel/QuoteViewModel.kt
+++ app/src/main/java/com/example/mvvm/ui/viewModel/QuoteViewModel.kt
@@ -2,18 +2,41 @@ package com.example.mvvm.ui.viewModel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.mvvm.data.model.QuoteModel
 import com.example.mvvm.data.model.QuoteProvider
+import com.example.mvvm.domain.GetQuotesUseCase
+import kotlinx.coroutines.launch
 
-class QuoteViewModel: ViewModel() {
+class QuoteViewModel : ViewModel() {
 
     // Define Live Data Mutable with Quote Model
     val quoteModel = MutableLiveData<QuoteModel>()
+    // Define Live Data Mutable with Boolean for Progress Bar
+    val isLoading = MutableLiveData<Boolean>()
+
+    // Call use case defined in the domain layer
+    var getQuoteUseCase = GetQuotesUseCase()
+
+    // Create onCreate Function to call the use case and assign values to live data
+    fun onCreate() {
+        viewModelScope.launch {
+            // Set on true progress bar process
+            isLoading.postValue(true)
+            val result = getQuoteUseCase()
+            if (!result.isNullOrEmpty()) {
+                quoteModel.postValue(result[0])
+                // Set on false progress bar after data loaded
+                isLoading.postValue(false)
+            }
+        }
+    }
 
     // Assign the quote Model to Mutable Live Data and Get Single quote of provider
-    fun randomQuote(){
-        val currentQuote = QuoteProvider.random()
-        quoteModel.postValue(currentQuote)
+    fun randomQuote() {
+//        val currentQuote = QuoteProvider.random()
+//        quoteModel.postValue(currentQuote)
     }
+
 
 }
